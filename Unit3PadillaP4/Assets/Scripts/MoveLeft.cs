9c9655b5ff1699ebb4e3616019503f35d81ee60d
deedough <nicholasomarpadilla@ca.rr.com>
2023-03-28 11:40:56 -0700

    did sum

--- Unit3PadillaP4/Assets/Scripts/MoveLeft.cs
+++ Unit3PadillaP4/Assets/Scripts/MoveLeft.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
+    private float speed = 30;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class MoveLeft : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
     }
 }
